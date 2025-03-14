f76106f11d7a3baa4dfc044b9001db383aa95b06
David <224d4811@live.uem.es>
2025-03-14 13:48:58 +0100

    Commit rama 1

--- src/clase/aleatorioa/Clase2.java
+++ src/clase/aleatorioa/Clase2.java
@@ -6,6 +6,7 @@ public class Clase2
 	public static void main(String[] args) 
 	{
 		System.out.println("Hola mundo");
+		System.out.println("Cambio rama1")
 		System.out.println("Hola a tod@s");
 	}
 
